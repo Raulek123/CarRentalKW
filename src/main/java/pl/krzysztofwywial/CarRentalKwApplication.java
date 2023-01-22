323b4e5f77ace4f1d3bb338620ed12ca46de4d33
Raulek123 <wywial.krzysztof@wp.pl>
2023-01-22 15:36:58 +0100

    Zmiana typu adnotacji pola type w klasie CarEntity z @Column na @Enumerated

--- src/main/java/pl/krzysztofwywial/CarRentalKwApplication.java
+++ src/main/java/pl/krzysztofwywial/CarRentalKwApplication.java
@@ -1,6 +1,5 @@
 package pl.krzysztofwywial;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/pl/krzysztofwywial/model/CarEntity.java
+++ src/main/java/pl/krzysztofwywial/model/CarEntity.java
@@ -11,40 +11,40 @@ import javax.persistence.*;
 @Getter
 @Setter
 @ToString
-    @Table(name = "CARS")
-    public class CarEntity implements Comparable<CarEntity> {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Table(name = "CARS")
+public class CarEntity implements Comparable<CarEntity> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(name = "brand")
-        private String brand;
+    @Column(name = "brand")
+    private String brand;
 
-        @Column(name = "model")
-        private String model;
+    @Column(name = "model")
+    private String model;
 
-        @Column(name = "year_of_production")
-        private int yearOfProduction;
+    @Column(name = "year_of_production")
+    private int yearOfProduction;
 
-        @Column(name = "type")
-        private CarType type;
+    @Enumerated(EnumType.ORDINAL)
+    private CarType type;
 
-        @Column(name = "image")
-        private String image;
+    @Column(name = "image")
+    private String image;
 
-        @Column(name = "available")
-        private boolean available;
+    @Column(name = "available")
+    private boolean available;
 
 
-        public String getImagePath() {
-            if (image == null || id == null) {
-                return null;
-            }
-                return "/images/" + id + "/" + image;
+    public String getImagePath() {
+        if (image == null || id == null) {
+            return null;
         }
-
-        @Override
-        public int compareTo(CarEntity c) {
-            return this.brand.compareToIgnoreCase(c.brand);
-        }
+        return "/images/" + id + "/" + image;
     }
+
+    @Override
+    public int compareTo(CarEntity c) {
+        return this.brand.compareToIgnoreCase(c.brand);
+    }
+}
